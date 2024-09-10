49542f491b63f0a688d7270172fd32f934b58307
Miraaj Kadam <miraajkadam@gmail.com>
2024-09-10 21:22:48 +0530

    add: worker service to create automation backup

---
+++ Bootcamp.AutomatedBackupService/Program.cs
@@ -0,0 +1,31 @@
+﻿using Bootcamp.Data;
+using Bootcamp.Data.Context;
+using Bootcamp.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Bootcamp.AutomatedBackupService
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<EngagementDbContext>(options =>
+                    options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Bootcamp2;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;MultipleActiveResultSets=true;")
+                    .LogTo(Console.WriteLine, LogLevel.Information));
+
+            serviceCollection.AddScoped<IEngagementRepository, EngagementRepository>()
+                .BuildServiceProvider();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var engagementRepository = serviceProvider.GetRequiredService<IEngagementRepository>();
+
+            var worker = new Worker(engagementRepository);
+
+            await worker.StartProcess();
+        }
+    }
+}
--- Bootcamp.Data/Interfaces/IEngagementRepository.cs
+++ Bootcamp.Data/Interfaces/IEngagementRepository.cs
@@ -6,6 +6,15 @@ namespace Bootcamp.Data.Interfaces
     public interface IEngagementRepository
     {
         Task<IEnumerable<Engagement>> GetAllEngagements();
+
         void AddEngagement(string clientName, DateTimeOffset auditStartDate, DateTimeOffset auditEndDate, int countryId, List<int> auditors, AuditTypeValue auditTypeId, EngagementStatusValue engagementStatusId);
+
+        void AddBackupSettings(string backupFrequency);
+
+        string? GetEngagementBackupFrequency();
+
+        EngagementBackup? GetEngagementBackupById(int id);
+
+        void PerformEngagementBackup(EngagementBackup? backup, bool shouldUpdate);
     }
 }
